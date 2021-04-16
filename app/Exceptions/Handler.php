25461f963c4f23e9322d7c46d7fb217ac25b8492
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2021-04-17 00:12:58 +0200

    Notfoundexception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +39,11 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function(NotFoundHttpException $e, Request $request ) {
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'No content'], 404);
+            }
+        } );
     }
 }
